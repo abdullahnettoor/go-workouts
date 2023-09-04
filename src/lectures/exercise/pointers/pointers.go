d88f8e9b477aeb12d021ca0508772c2e60cdb3e5
abdullahnettoor <abdullahnettoor@gmail.com>
2023-09-04 20:09:30 +0530

    👉🏽 Done an exercise of Pointer

--- src/lectures/exercise/pointers/pointers.go
+++ src/lectures/exercise/pointers/pointers.go
@@ -16,8 +16,61 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	active   = true
+	inactive = false
+)
+
+type SecurityTag bool
+
+type Item struct {
+	name string
+	tag  SecurityTag
+}
+
+func activateTag(tag *SecurityTag) {
+	*tag = active
+}
+
+func deactivateTag(tag *SecurityTag) {
+	*tag = inactive
+}
+
+func checkout(list []Item) {
+	for i, _ := range list {
+		deactivateTag(&list[i].tag)
+	}
+}
+
+func printItemsList(title string, list []Item) {
+	fmt.Println(title)
+	for _, item := range list {
+		fmt.Println(item.name, "\t:", item.tag)
+	}
+	fmt.Println()
+}
 
 func main() {
+
+	shirt := Item{"Shirt", active}
+	pants := Item{"Pants", active}
+	cap := Item{"Cap", active}
+	shoes := Item{"Shoes", active}
+
+	itemList := []Item{shirt, pants, cap}
+	itemList = append(itemList, shoes)
+
+	printItemsList("Items Added", itemList)
+
+	deactivateTag(&itemList[2].tag)
+
+	printItemsList("Cart Updated", itemList)
+
+	checkout(itemList)
+	printItemsList("Cheking Out ...", itemList)
 
 }
